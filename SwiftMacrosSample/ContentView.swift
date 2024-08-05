3b3d01c07a9e9dbb9205f64feb39923f913375cd
Rin-t <rin.t.4921@gmail.com>
2024-08-05 12:30:12 +0900

    Initial Commit

---
+++ SwiftMacrosSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftMacrosSample
+//
+//  Created by Rin on 2024/08/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftMacrosSample/SwiftMacrosSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMacrosSampleApp.swift
+//  SwiftMacrosSample
+//
+//  Created by Rin on 2024/08/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMacrosSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
